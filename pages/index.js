28ffef17f9be1ade2b752ebc6dba080ff4ad9501
Eyad Karim <eyad.karim.r3@gmail.com>
2023-05-20 20:55:47 -0700

    add responsive design

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import SOMP from '@/components/sections/SOMP';
 import HeroSection from './../components/sections/hero';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Home</title>
+      </Head>
       <HeroSection />
       <SOMP/>
     </>
