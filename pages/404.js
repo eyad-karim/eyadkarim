4357e81e00d735cbad00369ff8a0bc3da4cf278e
Eyad Karim <eyad.karim.r3@gmail.com>
2023-05-22 21:54:57 -0700

    add footer and 404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+function NotFound() {
+    return (<>
+        <section className="container mx-auto py-5">
+            <div className="flex justify-center my-80">
+                <div className="bg-light p-10 rounded-lg">
+                    404 | This page could not be found.
+                </div>
+            </div>
+        </section>
+    </>);
+}
+
+export default NotFound;
