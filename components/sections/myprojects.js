95dc7223bcbc0a25f17f99d24a6aad44594880d2
eyad-karim <128222718+eyad-karim@users.noreply.github.com>
2023-06-06 22:53:13 +0300

    Fix myprojects.js

--- components/sections/myprojects.js
+++ components/sections/myprojects.js
@@ -6,11 +6,11 @@ function MyProjects() {
             <h3 className="title">My projects</h3>
             <pre>
                 <span className="text-code-blue">const</span> <span className="text-code-sky">projects</span> = <span className="text-code-yellow">{"{"}</span><br/>
-                {"  "}<span className="text-code-sky">"Eyad website"</span> : <span className="text-code-brown">"Website talking about me and shows my projects."</span>,<br/>
-                {"  "}<span className="text-code-sky">"Tailwindcss project"</span> : <span className="text-code-brown">"project that I learn tailwindcss in, and test some classnames."</span>,<br/>
-                {"  "}<span className="text-code-sky">"Fake Windows project"</span> : <span className="text-code-brown">"fake windows os (beta)"</span>,<br/>
+                {"  "}<span className="text-code-sky">{"Eyad website"}</span> : <span className="text-code-brown">{"Website talking about me and shows my projects."}</span>,<br/>
+                {"  "}<span className="text-code-sky">{"Tailwindcss project"}</span> : <span className="text-code-brown">{"project that I learn tailwindcss in, and test some classnames."}</span>,<br/>
+                {"  "}<span className="text-code-sky">{"Fake Windows project"}</span> : <span className="text-code-brown">{"fake windows os (beta)"}</span>,<br/>
                 <span className="text-code-yellow">{"}"}</span><br/>
-                <span className="text-code-green">// if you want to see one of my projects you can check my github account in "about me" section</span>
+                <span className="text-code-green">// if you want to see one of my projects you can check my github account in {"about me"} section</span>
             </pre>
         </section>
     </>);
