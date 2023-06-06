70abf9a4cf93b23774611283d23f58e8a4e02780
eyad-karim <128222718+eyad-karim@users.noreply.github.com>
2023-06-06 22:56:22 +0300

    Fix myprojects.js

--- components/sections/myprojects.js
+++ components/sections/myprojects.js
@@ -10,7 +10,7 @@ function MyProjects() {
                 {"  "}<span className="text-code-sky">{"Tailwindcss project"}</span> : <span className="text-code-brown">{"project that I learn tailwindcss in, and test some classnames."}</span>,<br/>
                 {"  "}<span className="text-code-sky">{"Fake Windows project"}</span> : <span className="text-code-brown">{"fake windows os (beta)"}</span>,<br/>
                 <span className="text-code-yellow">{"}"}</span><br/>
-                <span className="text-code-green">// if you want to see one of my projects you can check my github account in {"about me"} section</span>
+                <span className="text-code-green">{"// if you want to see one of my projects you can check my github account in \"about me\" section"}</span>
             </pre>
         </section>
     </>);
